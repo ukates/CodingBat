83112cc1fe9df033f61204e444a5b71b9660c7a5
ukates <derrickcates@icloud.com>
2020-07-08 18:34:07 -0400

    Inital Commit

---
+++ WarmupOne/sleep_in.py
@@ -0,0 +1,15 @@
+"""The parameter weekday is True if it is a weekday,
+and the parameter vacation is True if we are on vacation.
+We sleep in if it is not a weekday or we're on vacation. Return True if we sleep in."""
+
+
+def sleep_in(weekday, vacation):
+    if not weekday:
+        return True
+    elif vacation:
+        return True
+    else:
+        return False
+
+
+print(sleep_in(True, False))
